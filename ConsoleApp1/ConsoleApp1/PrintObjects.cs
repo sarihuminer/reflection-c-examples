53409f8d836d7f932d3fb7e68831c21d08e7e794
sari shteren <sh6197174@gmail.com>
2021-10-20 13:42:01 +0300

    f

---
+++ ConsoleApp1/ConsoleApp1/PrintObjects.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class PrintObjects
+    {
+
+        public static void MyPrint(object o)
+        {
+
+            FieldInfo[] fieldInfo = null;
+            fieldInfo = o.GetType().GetFields();
+
+            foreach (var item in fieldInfo)
+            {
+                var prop = item.GetValue(o);
+                if (prop == null)
+                {
+                    Console.WriteLine(item.Name + ": NULL");
+                }
+                else
+                {
+                    if (prop.GetType().Equals(typeof(string)) || prop.GetType().IsPrimitive)
+                    {
+                        Console.WriteLine(item.Name + ": " + prop.ToString());
+                    }
+                    else
+                    {
+                        MyPrint(prop);
+                    }
+
+                }
+            }
+
+
+            //if (prop is IEnumerable)
+            //{
+            //    foreach (var listitem in prop as IEnumerable)
+            //    {
+            //        Console.WriteLine("Item: " + listitem.ToString());
+            //    }
+            //}
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Name n = new Name();
+            n.firstName = "Bill";
+            n.lastName = "Gates";
+            Person p = new Person();
+            p.age = 55;
+            p.name = n;
+
+            
+            //print simple class
+            // PrintObjects.MyPrint(n);
+            //print class that contain class 
+            // PrintObjects.MyPrint(p);
+        }
+    }
+}
